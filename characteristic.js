ed3b28ad18b9723bc97cf6e345460896c816b89a
Omkar Khair <Omkar Khair>
2016-03-21 23:55:19 +0530

    Basic BLE characteristics

---
+++ characteristic.js
@@ -0,0 +1,53 @@
+var util = require('util');
+var bleno = require('bleno');
+
+var BlenoCharacteristic = bleno.Characteristic;
+
+// Initialize BLE Characteristic
+var FirstCharacteristic = function() {
+  FirstCharacteristic.super_.call(this, {
+    uuid: 'fc0f',
+    properties: ['read', 'write', 'notify'],
+    value: null
+  });
+  this._value = new Buffer("OFF", "utf-8");
+  console.log("Characterisitic's value: "+this._value);
+  this._updateValueCallback = null;
+};
+
+// Inherit the BlenoCharacteristic
+util.inherits(FirstCharacteristic, BlenoCharacteristic);
+
+// BLE Read request
+FirstCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  console.log('FirstCharacteristic - onReadRequest: value = ' + this._value.toString("utf-8"), offset);
+  callback(this.RESULT_SUCCESS, this._value);
+};
+
+// BLE write request
+FirstCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+  this._value = data;
+  console.log('FirstCharacteristic - onWriteRequest: value = ' + this._value.toString("utf-8"));
+
+  if (this._updateValueCallback) {
+    console.log('FirstCharacteristic - onWriteRequest: notifying');
+
+    this._updateValueCallback(this._value);
+  }
+
+  callback(this.RESULT_SUCCESS);
+};
+
+// BLE subscribe
+FirstCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
+  console.log('FirstCharacteristic - onSubscribe');
+  this._updateValueCallback = updateValueCallback;
+};
+
+// BLE unsubscribe
+FirstCharacteristic.prototype.onUnsubscribe = function() {
+  console.log('FirstCharacteristic - onUnsubscribe');
+  this._updateValueCallback = null;
+};
+
+module.exports = FirstCharacteristic;
